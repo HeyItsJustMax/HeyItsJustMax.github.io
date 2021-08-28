b3481973f8229e3e5f3238ad9aabaa6c4f122772
Maximilian Rice <maximilian.rice1@gmail.com>
2021-08-28 13:26:13 -0400

    creates JS constants

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
+//=================
+//-----POKEDEX-----
+//=================
 
+// CONSTANTS - use $ to indicate you are using jquery
+
+const $name = $("#name")
+const $id = $("#index")
+const $types = $("#types")
+const $height = $("#height")
+const $weight = $("#weight")
+const $stats = $("#stats")
+const $healthPoints = $("#hp")
+const $attack = $("#attack")
+const $defense = $("#defense")
+const $specialAttack = $("#spclatk")
+const $specialDefense = $("#spcldfs")
+const $speed = $("#speed")
+const $moves = $("#moves")
 
 // Pokemon Name 1 - 151 (Bulbasaur - Mew)
 $.ajax({
