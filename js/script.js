3a3f15213aa2bd671c83dd48dbad3e3f33def1f2
Maximilian Rice <maximilian.rice1@gmail.com>
2021-08-28 12:57:11 -0400

    completes basic html setup

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('locked and loaded')
+
+
+// Pokemon Name 1 - 151 (Bulbasaur - Mew)
+$.ajax({
+    url:'https://pokeapi.co/api/v2/pokemon?limit=151/'
+  }).then(
+    function(data){
+     console.log(data);
+    },
+    function(error){
+     console.log('bad request', error);
+    }
+  );
+
