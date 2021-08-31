b005a949288d517d5cb6c100ba15ed4bd446f557
Maximilian Rice <maximilian.rice1@gmail.com>
2021-08-31 18:00:26 -0400

    JS file declares pokeTypes array

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 
 // CONSTANTS - use $ to indicate you are using jquery
 
+
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
 
 const $name = $("#name")
@@ -21,11 +22,13 @@ const $speed = $("#speed")
 const $moves = $("#moves")
 const $input = $('input[type="text"]')
 
+const pokeTypes = ["normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "fire", "water", "grass", "electric", "psychic", "ice", "dragon"]
+
 // Event Listeners
 
 $('form').on('submit', handleGetData)
 
-// Pokemon Name 1 - 151 (Bulbasaur - Mew)
+// FUNCTIONS
 
 function handleGetData(event) {
   event.preventDefault()
@@ -34,6 +37,7 @@ function handleGetData(event) {
   .then(function(data) {
       console.log(data)
       render(data)
+
     }, function(error) {
       console.log('bad request', error)
     })
@@ -42,17 +46,24 @@ function handleGetData(event) {
 function render(pokeData) {
 
     $name.text(pokeData.name)
-    $id.text()
-    $types.text()
-    $height.text()
-    $weight.text()
-    $stats.text()
-    $healthPoints.text()
-    $attack.text()
-    $specialAttack.text()
-    $specialDefense.text()
-    $speed.text()
-    $moves.text()
+    $id.text(pokeData.id)
+    if(pokeData.types.length > 1) {
+      $types.text(pokeData.types[0].type.name + ", " + pokeData.types[1].type.name)
+    } else {
+      $types.text(pokeData.types[0].type.name)
+    }
+    $height.text(pokeData.height)
+    $weight.text(pokeData.weight)
+    
+    $healthPoints.text(pokeData.stats[0].base_stat)
+    $attack.text(pokeData.stats[1].base_stat)
+    $defense.text(pokeData.stats[2].base_stat)
+    $specialAttack.text(pokeData.stats[3].base_stat)
+    $specialDefense.text(pokeData.stats[4].base_stat)
+    $speed.text(pokeData.stats[5].base_stat)
+    $moves.text(pokeData)
     
 
 }
+
+
