2a89ce64e21ea0b7c4b95082f88c634d94f62af9
Maximilian Rice <maximilian.rice1@gmail.com>
2021-09-02 20:05:40 -0400

    creates containers for CSS manipulation

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 
 
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
+const SPRITE_URL = ""
 
 const $name = $("#name")
 const $id = $("#index")
@@ -21,6 +22,8 @@ const $specialAttack = $("#spclatk")
 const $specialDefense = $("#spcldfs")
 const $speed = $("#speed")
 const $moves = $("#moves")
+const $sprites = $("#sprites")
+const $imgsrc = $('img[src=""]')
 const $input = $('input[type="text"]')
 
 const pokeTypes = ["normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "fire", "water", "grass", "electric", "psychic", "ice", "dragon"]
@@ -48,23 +51,32 @@ function render(pokeData) {
 
     $name.text(pokeData.name)
     $id.text(pokeData.id)
-    if(pokeData.types.length > 1) {
-      $types.text(pokeData.types[0].type.name + ", " + pokeData.types[1].type.name)
-    } else {
-      $types.text(pokeData.types[0].type.name)
-    }
+
+    // if(pokeData.types.length > 1) {
+    //   $types.text(pokeData.types[0].type.name + ", " + pokeData.types[1].type.name)
+    // } else {
+    //   $types.text(pokeData.types[0].type.name)
+    // }
+    $types.text(pokeData.types.reduce(function(acc, item, index){
+      if(index === 0){
+        return acc + item.type.name
+      } else {
+        return acc + ", " + item.type.name
+      }
+    },""))
+
     $height.text(pokeData.height)
     $weight.text(pokeData.weight)
-    
+
     $healthPoints.text(pokeData.stats[0].base_stat)
     $attack.text(pokeData.stats[1].base_stat)
     $defense.text(pokeData.stats[2].base_stat)
     $specialAttack.text(pokeData.stats[3].base_stat)
     $specialDefense.text(pokeData.stats[4].base_stat)
     $speed.text(pokeData.stats[5].base_stat)
-    $moves.text(pokeData)
-    
 
+    // $sprites.attr(pokeData.sprites.front_default)
+  
+    $("sprites").attr($imgsrc, pokeData.sprites.front_default)
 }
-
 
